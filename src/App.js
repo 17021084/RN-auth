53a62c0f12d446c211c1540098efb8520f1e93c0
17021084 <dotrunghg1999@gmail.com>
2021-02-11 11:02:31 +0700

    setup firebase auth

--- src/App.js
+++ src/App.js
@@ -10,7 +10,21 @@ import React, {Component} from 'react';
 import {StyleSheet, View, Text} from 'react-native';
 import {Header} from './components/common';
 
+import firebase from 'firebase';
+
 class App extends Component {
+  componentDidMount() {
+    //this will be hide in env. but this is'nt important
+    firebase.initializeApp({
+      apiKey: 'AIzaSyDTnIn585PpiG10GsbI7fBWb4Ze2DqY7RU',
+      authDomain: 'react-native-auth-app-8cd27.firebaseapp.com',
+      projectId: 'react-native-auth-app-8cd27',
+      storageBucket: 'react-native-auth-app-8cd27.appspot.com',
+      messagingSenderId: '563343592115',
+      appId: '1:563343592115:web:33c47861336405ee9a3ec6',
+      measurementId: 'G-KR2R739DTC',
+    });
+  }
   render() {
     return (
       <View style={styles.container}>
