7891c413ad661ad4a53e9bcc15cc7056b543195f
17021084 <dotrunghg1999@gmail.com>
2021-02-11 16:16:50 +0700

    complete login, auto sign up

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,9 +1,33 @@
 import React, {Component} from 'react';
 import {Text, TextInput, View} from 'react-native';
 import {ButtonLearnMore, Card, CardSection, Input} from './common';
+import firebase from 'firebase';
 
 export default class LoginForm extends Component {
-  state = {email: '', password: ''};
+  state = {email: '', password: '', error: '', success: ''};
+
+  onButtonPress() {
+    const {email, password} = this.state;
+    this.setState({error: '', success: ''});
+    firebase
+      .auth()
+      .signInWithEmailAndPassword(email, password)
+      .then(() => {
+        this.setState({success: 'logged'});
+      })
+      .catch(() => {
+        firebase
+          .auth()
+          .createUserWithEmailAndPassword(email, password)
+          .then(() => {
+            this.setState({success: 'Registed'});
+          })
+          .catch(() => {
+            this.setState({error: 'Authentication failed'});
+          });
+      });
+  }
+
   render() {
     return (
       <Card>
@@ -25,7 +49,15 @@ export default class LoginForm extends Component {
           />
         </CardSection>
         <CardSection>
-          <ButtonLearnMore>login</ButtonLearnMore>
+          <Text style={{color: 'red'}}>{this.state.error}</Text>
+        </CardSection>
+        <CardSection>
+          <Text style={{color: 'green'}}>{this.state.success}</Text>
+        </CardSection>
+        <CardSection>
+          <ButtonLearnMore onPress={this.onButtonPress.bind(this)}>
+            login
+          </ButtonLearnMore>
         </CardSection>
       </Card>
     );
