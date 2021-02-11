b792b82cc59880f9247d722469baf96b2ecb155a
17021084 <dotrunghg1999@gmail.com>
2021-02-11 19:18:14 +0700

    end

--- src/App.js
+++ src/App.js
@@ -8,12 +8,14 @@
 
 import React, {Component} from 'react';
 import {StyleSheet, View, Text} from 'react-native';
-import {Header} from './components/common';
+import {ButtonLearnMore, Header, Spinner} from './components/common';
 
 import firebase from 'firebase';
 import LoginForm from './components/LoginForm';
 
 class App extends Component {
+  state = {isLogged: null};
+
   // we should use hook instead. because is to outdate
   componentWillMount() {
     //this will be hide in env. but this is'nt important
@@ -26,12 +28,42 @@ class App extends Component {
       appId: '1:563343592115:web:33c47861336405ee9a3ec6',
       measurementId: 'G-KR2R739DTC',
     });
+    firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        console.log('login roi');
+        this.setState({isLogged: true});
+      } else {
+        console.log('chua login');
+        this.setState({isLogged: false});
+      }
+    });
   }
+
+  logOut() {
+    firebase.auth().signOut();
+  }
+
+  renderContent() {
+    switch (this.state.isLogged) {
+      case true:
+        return (
+          <ButtonLearnMore onPress={this.logOut.bind(this)}>
+            Logout
+          </ButtonLearnMore>
+        );
+      case false:
+        return <LoginForm />;
+
+      default:
+        return <Spinner />;
+    }
+  }
+
   render() {
     return (
       <View style={styles.container}>
         <Header headerText={'authApp'} />
-        <LoginForm></LoginForm>
+        {this.renderContent()}
       </View>
     );
   }
